f5d3e7a63471b1eb3a96b025600ab90896cef3f5
SCHTAMP <ylyka0@ya.ru>
2020-08-12 00:49:55 +0300

    Молоточки

--- src/lfp_nato.lythium/ACEX_Fortify_Presets.hpp
+++ src/lfp_nato.lythium/ACEX_Fortify_Presets.hpp
@@ -1,16 +1,12 @@
 class ACEX_Fortify_Presets {
     class small {
-        displayName = CSTRING(small);
+        displayName = "Custom";
         objects[] = {
 			{"Land_BagFence_Corner_F", 3},
             {"Land_BagFence_Round_F", 3},
             {"Land_BagFence_Short_F", 2},
             {"Land_BagFence_Long_F", 4},
-            {"Land_Plank_01_4m_F", 2},
-			{"Land_HBarrierTower_F", 25},
-			{"Land_BagBunker_Large_F", 25},
-			{"Land_HBarrier_5_F", 15},
-			{"Land_Cargo_Patrol_V3_F", 20}
+            {"Land_Plank_01_4m_F", 2}
         };
     };
 };
