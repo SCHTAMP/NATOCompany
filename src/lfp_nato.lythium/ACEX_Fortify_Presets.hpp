d4d894043da45e5e4b09a2aac2d9b346ba0d6a26
SCHTAMP <ylyka0@ya.ru>
2020-08-14 00:10:09 +0300

    Увеличил бюджет до 350, поменял/добавил молоточки

--- src/lfp_nato.lythium/ACEX_Fortify_Presets.hpp
+++ src/lfp_nato.lythium/ACEX_Fortify_Presets.hpp
@@ -4,9 +4,13 @@ class ACEX_Fortify_Presets {
         objects[] = {
 			{"Land_BagFence_Corner_F", 3},
             {"Land_BagFence_Round_F", 3},
-            {"Land_BagFence_Short_F", 2},
             {"Land_BagFence_Long_F", 4},
-            {"Land_Plank_01_4m_F", 2}
+            {"Land_Plank_01_4m_F", 2},
+            {"Land_SandbagBarricade_01_hole_F", 10},
+            {"Land_SandbagBarricade_01_half_F", 8},
+            {"Land_SandbagBarricade_01_F", 10}, 
+            {"Land_ConcreteHedgehog_01_F", 5},
+            {"Land_BagBunker_Small_F",20}
         };
     };
 };
